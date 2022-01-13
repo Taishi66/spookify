436de211a076936647250e80ca314a70d7195b81
jclamy <jctlamy@gmail.com>
2022-01-13 21:03:08 +0100

    fixed unmount component issue

--- src/App.js
+++ src/App.js
@@ -9,43 +9,66 @@ import { useDataLayerValue } from "./DataLayer"
 const spotify = new SpotifyWebApi();
 
 function App() {
-
   const [{ user, token }, dispatch] = useDataLayerValue();
 
   //run code based on a given condition
   useEffect(() => {
+
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const _token = hash.access_token;
-
+    let _token = hash.access_token;
     console.log("token =>>>> ", token);
 
     if (_token) {
-      dispatch({
-        type: "SET_TOKEN",
-        token: _token
-      });
-
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user => {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.getPlaylist("37i9dQZEVXcCeFtBHuvCVW?gtm").then((response) =>
         dispatch({
-          type: 'SET_USER',
-          user: user
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
         });
-      })
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
     }
-    console.log("TOKEN>>>>", token);
-  }, [dispatch, token]);
+  }, [token, dispatch]);
   console.log("USER>>>", user);
 
 
   return (
-    //BEM
     <div className="app">
-      {token ? <Player spotify={spotify} /> : <Login />}
-    </div >
+      {!token && <Login />}
+      {token && <Player spotify={spotify} />}
+    </div>
   );
 }
-
 export default App;
--- src/components/pages/player/Sidebar.js
+++ src/components/pages/player/Sidebar.js
@@ -1,11 +1,15 @@
 import "./Sidebar.css"
 import React from 'react'
+import { useDataLayerValue } from "../../../DataLayer"
 import SidebarOption from './SidebarOption'
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 
 function Sidebar() {
+    const [{ playlists }, dispatch] = useDataLayerValue();
+    console.log(playlists);
+
     return (
         <div className="sidebar">
             <img
@@ -13,14 +17,17 @@ function Sidebar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
-            <SidebarOption Icon={HomeIcon} option="Home" />
-            <SidebarOption Icon={SearchIcon} option="Search" />
-            <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
             <br />
             <strong className="sidebar__title">PLAYLISTS</strong>
             <hr />
+            {playlists?.items?.map((playlist) => (
+                <SidebarOption title={playlist.name} />
+            ))}
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
