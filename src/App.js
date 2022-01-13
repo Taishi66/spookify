0bba1cee303584b969b9c1f2ef64d9e1b2a8be8c
jclamy <jctlamy@gmail.com>
2022-01-13 15:33:19 +0100

    npm i spotify-web-api-js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import Login from '../src/components/pages/login/Login'
-
+import { getTokenFromUrl } from './lib/spotify'
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+
+  //run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    console.log("token =>>>> ", token);
+
+    if (_token) {
+      setToken(_token);
+    }
+  });
+
+
   return (
     //BEM
     <div className="app">
+      {
+        token ? (
+          <h1>I'm logged in </h1>
+          // <Player />
+        ) : [
+          <Login />
+        ]
+      }
       <Login />
     </div >
   );
--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -21,7 +21,19 @@ const scopes = [
     "user-read-currently-playing",
     "user-read-recently-played",
     "user-follow-read",
-]
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            //access token
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {})
+}
 
 export const loginUrl =
     `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
