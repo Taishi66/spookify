6d073b210be5eca98721dda48f8ad64b0c8a8bc9
jclamy <jctlamy@gmail.com>
2022-01-13 17:51:15 +0100

    add material ui icon

--- src/components/pages/player/Sidebar.js
+++ src/components/pages/player/Sidebar.js
@@ -1,5 +1,9 @@
 import "./Sidebar.css"
 import React from 'react'
+import SidebarOption from './SidebarOption'
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 
 function Sidebar() {
     return (
@@ -9,7 +13,12 @@ function Sidebar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
-
+            <SidebarOption Icon={HomeIcon} option="Home" />
+            <SidebarOption Icon={SearchIcon} option="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
         </div>
     )
 }
