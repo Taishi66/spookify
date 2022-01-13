921f3a136a4376c1939623a95cf9799660363885
jclamy <jctlamy@gmail.com>
2022-01-13 15:04:04 +0100

    add login.js && set ./lib/spotify.js

---
+++ src/lib/spotify.js
@@ -0,0 +1,27 @@
+//"https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000"
+
+const clientId = "122015077f984db08676c3782fa702ff"
+
+const scopes = [
+
+    "user-read-private",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "user-read-email",
+    "streaming",
+    "user-library-read",
+    "user-top-read",
+    "user-top-read",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-follow-read",
+]
+
+export const loginUrl =
+    `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
